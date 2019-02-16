049f8a7c1b0dbe3b9fe9efb9550f65aa99155755
Amber Sharma <shrama.amber.p@gmail.com>
2019-02-15 19:11:03 -0800

    readded test

--- client/componentTests/ProfileImage.test.js
+++ client/componentTests/ProfileImage.test.js
@@ -3,9 +3,9 @@ import React from 'react';
 import ProfileImage from '../ProfileImage';
 
 
-xdescribe('<ProfileImage/>', () => {
+describe('<ProfileImage/>', () => {
   it('has a prop called src', () => {
-    const wrapper = mount(<ProfileImage src={'url("http://placekitten.com/50/50")'} />);
+    const wrapper = shallow(<ProfileImage src={'url("http://placekitten.com/50/50")'} />);
     expect(wrapper.find(ProfileImage)).toHaveProp('src');
   })
 });
